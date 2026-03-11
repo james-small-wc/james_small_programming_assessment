61eac03993beb28f1abd6a1c571b2a1535421083
james-small-wc <23416@wc.school.nz>
2026-03-12 11:57:45 +1300

    planning

---
+++ main.py
@@ -0,0 +1,9 @@
+#planning
+#name is language racer
+#main menu with season set out
+#end goal is to win championship
+#asked questions during race (language)
+#car upgrades
+#different tracks with different attributes
+#rival tally
+#different difficultys

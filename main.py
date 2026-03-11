6ccdebaa08cd0a6974a46d274220e4bec281ef32
james-small-wc <23416@wc.school.nz>
2026-03-12 12:27:11 +1300

    Welcome and main menu

--- main.py
+++ main.py
@@ -1,9 +1,35 @@
-#planning
-#name is language racer
+player_name=""
+round_num=0
+rounds=["Wellington Waterfront","Newtown"]
+
+def welcome():
+    global player_name
+    global round_num
+    print("Welcome to Language Racer!")
+    print("")
+    player_name=input("What is your name?")
+    print("")
+    print(f"Congratulations on starting your racing adventure {player_name}!")
+    print("")
+    round_num=1
+    main_menu()
+def main_menu():
+    x=0
+    print("Rounds:")
+    print("")
+    for i in rounds:
+        print(rounds[x])
+        x+=1
+    print("")
+    print(f"You are on Round {round_num}: {rounds[round_num-1]}")
+
+
+welcome()
+
 #main menu with season set out
 #end goal is to win championship
 #asked questions during race (language)
 #car upgrades
 #different tracks with different attributes
 #rival tally
-#different difficultys
+#different difficulty
